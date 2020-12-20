e65828d2efeb043dc6aaca22ae3e05c80bd5d395
rdtank <rdtank112@gmail.com>
2020-12-20 18:31:25 +0530

    Database Setup

---
+++ Source Control Final Assignment/Source Control Assignment/Source Control Assignment/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Source_Control_Assignment.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source Control Final Assignment/Source Control Assignment/Source Control Assignment/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Source_Control_Assignment.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Age { get; set; }
+        public string Image { get; set; }
+    }
+}
